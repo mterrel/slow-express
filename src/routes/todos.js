ba6578f2f96a50bc2b18ca7ea13292ab14e1d315
Mark Terrel <mark@unbounded.systems>
2022-07-19 21:41:39 -0600

    BREAK: throw an error on get

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -51,7 +51,7 @@ router.post("/", asyncMiddleware(async (req, res) => {
 
 router.get('/', asyncMiddleware(async (req, res) => {
   const todos = await prisma.TodoItem.findMany();
-  process.exit(1);
+  throw new Error("Oops");
   res.json(todos);
 }));
 
